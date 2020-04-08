79d34c9d9c2e3667f025f7f880a08a99972b8cfc
JMan <60026263+JMantis0@users.noreply.github.com>
2020-04-08 04:14:35 -0700

    Added comments to js to help explain to myself what code is doing
    Created function validatePassWordLength and placed code created in progress section 1 into its block.
    Created function validateIsANumber which validates that user input is actually a number.
    Placed function calls within existing function generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment Code
+
+//#generate referes to the button's ID on line 38 of HTML
+//document.querySelector() returns the first element within the document that matches the selector inside the argument
+//So this line below  is assigning the <button> element on line 38 of the HTML to the variable generateBtn
 let generateBtn = document.querySelector("#generate");
 
 
@@ -10,39 +14,74 @@ let generateBtn = document.querySelector("#generate");
 //********************************************************************************
 //********************************************************************************
 
-let passWordLength = prompt("Enter number of characters in new password (min 8 max 128)");
 
-//Nested loop recurrs if passWordLength is not between 7 and 129
-while (passWordLength < 8 || passWordLength > 128) {
-	
-	if(passWordLength < 8) {
+//Function that creates the new password.  Is called upon click of the big red button.
+function generatePassword() {
 
-		alert("Password must be at least 8 characters.");
-		passWordLength = prompt("Enter number of characters in new password (min 8 max 128)");
-
-	}
-	else if(passWordLength > 128) {
-
-		alert("Password must be less than 128 characters.");
-		passWordLength = prompt("Enter number of characters in new password (min 8 max 128)");
-
+	let passWordLength = validatePassWordLength(validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)")));
 	
 
 }
 
+//Function validates that the input for character length is actually a number
+function validateIsANumber(isThisANumber) {
 
+	//Number() won't say a blank space is a NaN but parseInt() does
+	//ParseInt won't say any string starting with a number is a NaN Number() does ie: "9 apples" or "30 years"
+	//Both are used to keep any old string with a number at the beginning and blank spaces from
+	//slipping by.
 
+	while(isNaN(parseInt(isThisANumber)) || isNaN(Number(isThisANumber))) {
+
+		alert("Password length must be a number");
+
+		isThisANumber = prompt("Enter number of characters in new password (min 8 max 128)");
+
+	}
+
+	let numberForSure = isThisANumber;
+
+	return numberForSure;
+
+}
+
+//Function validates that password length lies in range 8-128
+function validatePassWordLength(length) {
+	
+	//Nested loop recurrs if passWordLength is not between 7 and 129
+	while (length < 8 || length > 128) {
+
+		if (length < 8) {
+
+			alert("Password must be at least 8 characters.");
+			length = validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)"));
+
+		}
+		else if (length > 128) {
+
+			alert("Password must be less than 128 characters.");
+			length = validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)"));	
+		}
+
+	}
+
+	return length;
+
+}
 
 
 // Write password to the #password input
 function writePassword() {
-	
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+	let password = generatePassword();
+	//similar to the querySelector on line 6, line 48 assigns the <text> element on line 32 of the HTML to variable passwordText
+	let passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+// Remember above we assigned the HTML button on line 38 of the HTML to variable generateBtn
+// This line below makes it so when that button is clicked, the writePassword() function is called
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -4,22 +4,31 @@ The goal of this project is to create an application that generates a random pas
 
 
 * GIVEN I need a new, secure password
-* * WHEN I click the button to generate a password
-* THEN I am presented with a series of prompts for password criteria
-* WHEN prompted for password criteria
-* THEN I select which criteria to include in the password
-* WHEN prompted for the length of the password
-* THEN I choose a length of at least 8 characters and no more than 128 characters
-* WHEN prompted for character types to include in the password
-* THEN I choose lowercase, uppercase, numeric, and/or special characters
-* WHEN I answer each prompt
-* THEN my input should be validated and at least one character type should be selected
-* WHEN all prompts are answered
-* THEN a password is generated that matches the selected criteria
-* WHEN the password is generated
-* THEN the password is either displayed in an alert or written to the page
+* *  WHEN I click the button to generate a password
+* * * THEN I am presented with a series of prompts for password criteria
+* * WHEN prompted for password criteria
+* * * THEN I select which criteria to include in the password
+* * WHEN prompted for the length of the password
+* * * THEN I choose a length of at least 8 characters and no more than 128 characters
+* * WHEN prompted for character types to include in the password
+* * * THEN I choose lowercase, uppercase, numeric, and/or special characters
+* * WHEN I answer each prompt
+* * * THEN my input should be validated and at least one character type should be selected
+* * WHEN all prompts are answered
+* * * THEN a password is generated that matches the selected criteria
+* * WHEN the password is generated
+* * * THEN the password is either displayed in an alert or written to the page
 
 
 ## Progress
 
+1) 4/7/2020 
+
 The first task I completed is to write code that collects the number of characters the user wishes his/her new password to be, and to throw an error  and let the user retry if the number does not meet acceptance criteria (at least 8, max 128).
+
+2) 4/8/2020
+Added comments to js to help explain to myself what code is doing
+
+Created function validatePassWordLength and placed code created in progress section 1 into its block.
+Created function validateIsANumber which validates that user input is actually a number.
+Placed function calls within existing function generatePassword()
