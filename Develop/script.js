edb570edad966cd0ff968b67631c0b8f516518cd
JMan <60026263+JMantis0@users.noreply.github.com>
2020-04-09 11:34:17 -0700

    4) 4/9/2020
    * Created function  which calls critera-gathering functions for character types and utilizes an if/then within a while-loop to ensure the user includes at least one character type.
    
    git commit -m 4/9/2020

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,14 @@
-// Assignment Code
-
-//  #generate referes to the button's ID on line 38 of HTML
-//  document.querySelector() returns the first element within the document that matches the selector inside the argument
-//  So this line below  is assigning the <button> element on line 38 of the HTML to the variable generateBtn
 let generateBtn = document.querySelector("#generate");
 
 let passwordLength = 0;
-let lowercase = false;
-let uppercase = false;
-let numeric = false;
-let special = false;
+let lowercase = true;
+let uppercase = true;
+let numeric = true;
+let special = true;
+let noneSelected = true;
 
-
-
-//  Next prompt user for other criteria, lowercase, uppercase, numeric, specials.
+//  Following 4 functions prompt user to choose the types of characters
+//  to be included in the password:
 
 function includeLowercase() {
 
@@ -41,30 +36,55 @@ function includeSpecial() {
 	let confirmSpecial = confirm("Click OK to include special characters.  Otherwise, click Cancel");
 	return confirmSpecial;
 }
+
+//  Function selectCharacters() calls above functions from within while-loop.
+//  Logic causes while-loop to recur unless user confirms at least one character type
+
+function selectCharacters() {
+
+	while(noneSelected) {
+	
+		lowercase = includeLowercase();
+		uppercase = includeUppercase();
+		numeric = includeNumeric();
+		special = includeSpecial();
+
+		if(!lowercase && !uppercase && !numeric && !special) {
+
+			alert("You must include at least one character type.");
+
+		}
+		else {
+
+			noneSelected = false;
+
+		}
+
+	}
+
+}
 	
 
-//  Function that creates the new password.  Is called upon click of the big red button.
+//  Function generatePassword creates the new password.  Is called upon click of the big red button.
 function generatePassword() {
 
+	noneSelected = true;
 	passwordLength = validatePasswordLength(validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)")));
-	lowercase = includeLowercase();
-	uppercase = includeUppercase();
-	numeric = includeNumeric();
-
+	selectCharacters();
 	consolePrintout();
 
 	
 }
 
-//  Function validates that the input for character length is actually a number
+//  Function validateIsANumber tests whether user input is actually a number
+//  Logic prompts user to enter new character length if entry is not a number
 function validateIsANumber(isThisANumber) {
 
 	//  Number() won't say a blank space is a NaN but parseInt() does.
-	//  ParseInt won't say any string starting with a number is a NaN Number() does 
+	//  ParseInt() won't say any string starting with a number is a NaN Number() does 
 	//              ie: "9 apples" or "30 years"
-	//  Both are used to keep any old string with a number at the beginning and blank spaces from
-	//  slipping by.
-
+	//  Logic uses both together to prevent a non-numeric string starting with a number 
+	//  or a blank space from being allowed as a password length.
 	while(isNaN(parseInt(isThisANumber)) || isNaN(Number(isThisANumber))) {
 
 		alert("Password length must be a number");
@@ -78,18 +98,11 @@ function validateIsANumber(isThisANumber) {
 
 }
 
-//********************************************************************************
-//********************************************************************************
-//*****  TASK:  Get the number of characters the password should contain.    *****
-//*****  if number is less than 8 or greater than 128 throw error and give   *****
-//*****  another prompt    **COMPLETE**                                      *****
-//********************************************************************************
-//********************************************************************************
 
-//  Function validates that password length lies in range 8-128
+//  Function validatesPasswordLength() tests whether length lies in range 8-128
+//  Logic prompts User to enter a new length if it lies outsite range 8-128
 function validatePasswordLength(length) {
 	
-	//  Nested loop recurrs if length lies outside range 8-128
 	while (length < 8 || length > 128) {
 
 		if (length < 8) {
@@ -127,6 +140,8 @@ function writePassword() {
 // This line below makes it so when that button is clicked, the writePassword() function is called
 generateBtn.addEventListener("click", writePassword);
 
+
+// Function consolePrintout() assists developer in testing and debugging
 function consolePrintout() {
 
 	console.log("passwordLength is = " + passwordLength);
@@ -134,5 +149,6 @@ function consolePrintout() {
 	console.log("uppercase is = " + uppercase);
 	console.log("numeric is = " + numeric);
 	console.log("special is = " + special);
+	console.log("noneSelected is = " + noneSelected);
 
 }
