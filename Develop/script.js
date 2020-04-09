da7552190f37de3212e357ce29b4e7fdca7d4ddf
JMan <60026263+JMantis0@users.noreply.github.com>
2020-04-09 10:02:07 -0700

    3) 4/9/2020
    * Added variable initialization set to hold booleans variables (representing which types of characters to include in password) and password length (length, lowercase, uppercase, numeric, and special).
    
    * Created functions to gather desired criteria from user confirmUppercase(), confirmLowercase(), confirmNumeric(), confirmSpecial().

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,69 @@
 // Assignment Code
 
-//#generate referes to the button's ID on line 38 of HTML
-//document.querySelector() returns the first element within the document that matches the selector inside the argument
-//So this line below  is assigning the <button> element on line 38 of the HTML to the variable generateBtn
+//  #generate referes to the button's ID on line 38 of HTML
+//  document.querySelector() returns the first element within the document that matches the selector inside the argument
+//  So this line below  is assigning the <button> element on line 38 of the HTML to the variable generateBtn
 let generateBtn = document.querySelector("#generate");
 
-
-//********************************************************************************
-//********************************************************************************
-//*****  TASK:  Get the number of characters the password should contain.    *****
-//*****  if number is less than 8 or greater than 128 throw error and give   *****
-//*****  another prompt    **COMPLETE**                                      *****
-//********************************************************************************
-//********************************************************************************
+let passwordLength = 0;
+let lowercase = false;
+let uppercase = false;
+let numeric = false;
+let special = false;
 
 
-//Function that creates the new password.  Is called upon click of the big red button.
-function generatePassword() {
 
-	let passWordLength = validatePassWordLength(validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)")));
-	
+//  Next prompt user for other criteria, lowercase, uppercase, numeric, specials.
+
+function includeLowercase() {
+
+	let confirmLowercase = confirm("Click OK to include lowercase characters.  Otherwise, click Cancel.");
+	return confirmLowercase;
 
 }
 
-//Function validates that the input for character length is actually a number
+function includeUppercase() {
+
+	let confirmUppercase = confirm("Click OK to include uppercase characters.  Otherwise, click Cancel.");
+	return confirmUppercase;
+
+}
+
+function includeNumeric() {
+
+	let confirmNumeric = confirm("Click OK to include numeric characters.  Otherwise, click Cancel.");
+	return confirmNumeric;
+
+}
+
+function includeSpecial() {
+	
+	let confirmSpecial = confirm("Click OK to include special characters.  Otherwise, click Cancel");
+	return confirmSpecial;
+}
+	
+
+//  Function that creates the new password.  Is called upon click of the big red button.
+function generatePassword() {
+
+	passwordLength = validatePasswordLength(validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)")));
+	lowercase = includeLowercase();
+	uppercase = includeUppercase();
+	numeric = includeNumeric();
+
+	consolePrintout();
+
+	
+}
+
+//  Function validates that the input for character length is actually a number
 function validateIsANumber(isThisANumber) {
 
-	//Number() won't say a blank space is a NaN but parseInt() does
-	//ParseInt won't say any string starting with a number is a NaN Number() does ie: "9 apples" or "30 years"
-	//Both are used to keep any old string with a number at the beginning and blank spaces from
-	//slipping by.
+	//  Number() won't say a blank space is a NaN but parseInt() does.
+	//  ParseInt won't say any string starting with a number is a NaN Number() does 
+	//              ie: "9 apples" or "30 years"
+	//  Both are used to keep any old string with a number at the beginning and blank spaces from
+	//  slipping by.
 
 	while(isNaN(parseInt(isThisANumber)) || isNaN(Number(isThisANumber))) {
 
@@ -40,15 +74,22 @@ function validateIsANumber(isThisANumber) {
 	}
 
 	let numberForSure = isThisANumber;
-
 	return numberForSure;
 
 }
 
-//Function validates that password length lies in range 8-128
-function validatePassWordLength(length) {
+//********************************************************************************
+//********************************************************************************
+//*****  TASK:  Get the number of characters the password should contain.    *****
+//*****  if number is less than 8 or greater than 128 throw error and give   *****
+//*****  another prompt    **COMPLETE**                                      *****
+//********************************************************************************
+//********************************************************************************
+
+//  Function validates that password length lies in range 8-128
+function validatePasswordLength(length) {
 	
-	//Nested loop recurrs if passWordLength is not between 7 and 129
+	//  Nested loop recurrs if length lies outside range 8-128
 	while (length < 8 || length > 128) {
 
 		if (length < 8) {
@@ -60,21 +101,21 @@ function validatePassWordLength(length) {
 		else if (length > 128) {
 
 			alert("Password must be less than 128 characters.");
-			length = validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)"));	
+			length = validateIsANumber(prompt("Enter number of characters in new password (min 8 max 128)"));
+
 		}
 
 	}
-
+	
 	return length;
 
 }
-
 
 // Write password to the #password input
 function writePassword() {
 
 	let password = generatePassword();
-	//similar to the querySelector on line 6, line 48 assigns the <text> element on line 32 of the HTML to variable passwordText
+	//  similar to the querySelector on line 6, line 48 assigns the <text> element on line 32 of the HTML to variable passwordText
 	let passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
@@ -85,3 +126,13 @@ function writePassword() {
 // Remember above we assigned the HTML button on line 38 of the HTML to variable generateBtn
 // This line below makes it so when that button is clicked, the writePassword() function is called
 generateBtn.addEventListener("click", writePassword);
+
+function consolePrintout() {
+
+	console.log("passwordLength is = " + passwordLength);
+	console.log("lowercase is = " + lowercase);
+	console.log("uppercase is = " + uppercase);
+	console.log("numeric is = " + numeric);
+	console.log("special is = " + special);
+
+}
